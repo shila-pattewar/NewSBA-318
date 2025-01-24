eded61923b6a06166bd13652a6962caf1ee243c6
shila <shilapattewar16@gmail.com>
2025-01-23 18:19:09 -0600

    post delete and patch

---
+++ data/protin.js
@@ -0,0 +1,32 @@
+const protien= [
+    {
+      "name": "Chicken",
+      "type": "Meat",
+      "serving_size": "100g",
+      "protein_content": "31g"
+    },
+    {
+      "name": "Beef",
+      "type": "Meat",
+      "serving_size": "100g",
+      "protein_content": "26g"
+    },
+    {
+      "name": "Tofu",
+      "type": "Plant-based",
+      "serving_size": "100g",
+      "protein_content": "8g"
+    },
+    {
+      "name": "Fish",
+      "type": "Seafood",
+      "serving_size": "100g",
+      "protein_content": "22g"
+    },
+    {
+      "name": "Eggs",
+      "type": "Animal-based",
+      "serving_size": "1 large egg",
+      "protein_content": "6g"
+    }
+  ]
--- routes/fruits.js
+++ routes/fruits.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const fruitsData = require("../data/fruits");
 const fruits = require("../data/fruits");
 
 
 // GET Data
 router.get('/', (req, res) => {
    // res.send('This is fruits page');
-    res.json(fruitsData)
+    res.json(fruits)
     console.log("You are on fruit page")
 })
 
 
 // POST Data (to add a new fruit)
 router.use(express.json());
-
 
 
 router.post('/', (req, res) => {
@@ -26,14 +24,14 @@ router.post('/', (req, res) => {
     if (req.body.name && req.body.color && req.body.price) {
         // Create a new fruit object
         const newFruit = {
-            id: fruitsData.length ? fruitsData[fruitsData.length - 1].id + 1 : 1, 
+            id: fruits.length ? fruits[fruits.length - 1].id + 1 : 1, 
             name: req.body.name,
             color: req.body.color,
             price: req.body.price
         };
 
-        // Add the new fruit to the fruitsData array
-        fruitsData.push(newFruit);
+        // Add the new fruit to the fruits array
+        fruits.push(newFruit);
 
         // Send a response with the new fruit object
         res.status(201).json(newFruit);  // 201 status code for created resource
@@ -43,8 +41,49 @@ router.post('/', (req, res) => {
     }
 });
 
+// PATCH
 
+router.patch("/:id", (req, res, next) => {
 
+    console.log(req.body);
+    
+    const patchFruit = fruits.find((patchFruit, i) => {
+
+        console.log(`This is the fruit id: ${patchFruit.id}`);
+        console.log(`This is the req.params.id: ${req.params.id}`);
+
+      if (patchFruit.id == req.params.id) {
+        for (const key in req.body) {
+
+          console.log(`Updating field ${key} to ${req.body[key]}`);
+
+          fruits[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+
+    if (patchFruit)
+            res.json(patchFruit);
+    else 
+        {
+            next();
+        }
+    });
+  router.delete("/:id", (req, res, next) => {
+    // The DELETE request route simply removes a resource.
+    console.log(req.body)
+    const deleteFruit = fruits.find((deleteFruit, i) => {
+      if (deleteFruit.id == req.params.id)
+         {
+            fruits.splice(i, 1);
+            return true;
+         }
+    });
+
+    if (deleteFruit) res.json(deleteFruit);
+    else next();
+  });
 
 
 module.exports = router;
